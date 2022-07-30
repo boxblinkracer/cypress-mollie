269854951be8ac55d2c70045b6af951aeec441ad
Christian Dangl <boxblinkracer@gmx.net>
2022-07-30 19:35:44 +0200

    add more payment methods to list screen

--- src/actions/screens/PaymentListScreen.js
+++ src/actions/screens/PaymentListScreen.js
@@ -7,5 +7,96 @@ export default class PaymentListScreen {
         cy.contains('PayPal').click();
     }
 
+    /**
+     *
+     */
+    selectCreditCard() {
+        cy.contains('Credit/Debit Card').click();
+    }
+
+    /**
+     *
+     */
+    selectGiropay() {
+        cy.contains('giropay').click();
+    }
+
+    /**
+     *
+     */
+    selectPayLater() {
+        cy.contains('Pay later').click();
+    }
+
+    /**
+     *
+     */
+    selectPayNow() {
+        cy.contains('Pay now').click();
+    }
+
+    /**
+     *
+     */
+    selectSOFORT() {
+        cy.contains('SOFORT').click();
+    }
+
+    /**
+     *
+     */
+    selectiDEAL() {
+        cy.contains('iDEAL').click();
+    }
+
+    /**
+     *
+     */
+    selectEPS() {
+        cy.contains('eps').click();
+    }
+
+    /**
+     *
+     */
+    selectBankTransfer() {
+        cy.contains('Bank transfer').click();
+    }
+
+    /**
+     *
+     */
+    selectMyBank() {
+        cy.contains('MyBank').click();
+    }
+
+    /**
+     *
+     */
+    selectBancontact() {
+        cy.contains('Bancontact').click();
+    }
+
+    /**
+     *
+     */
+    selectP24() {
+        cy.contains('Przelewy24').click();
+    }
+
+    /**
+     *
+     */
+    selectBelfius() {
+        cy.contains('Belfius').click();
+    }
+
+    /**
+     *
+     */
+    selectSepaDirectDebit() {
+        cy.contains('SEPA Direct Debit').click();
+    }
+
 }
 
